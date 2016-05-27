1d13c72a11a61f698a81ae2ad5084f3fc9800e39
pnamgal <n96746@gmail.com>
2016-05-27 20:57:06 -0300

    modelos de alumnos actualizado

--- alumno/models.py
+++ alumno/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+Class Alumno(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=30)
+    dni = models.IntegerField()
