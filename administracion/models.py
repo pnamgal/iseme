4fddce94f0c955ab18fa5bcf0405f8f8c99922e6
pabli7o <pablito_dpunta@hotmail.com>
2016-05-28 15:34:40 -0300

    Modificaciones
    Se agrego el template registrar y se elimnaron las app administrador y
    alumnos

--- administracion/models.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-
-from django.db import models
-
-Class Administrador(models.Model):
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    direccion = models.CharField(max_length=50)
-    mail = models.EmailField(max_length=50)
