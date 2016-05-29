2484706aae7dadbc3b5e75fb5b8ed42d3ed7125b
pabli7o <pablito_dpunta@hotmail.com>
2016-05-29 16:33:55 -0300

    cam

--- userprofiles/models.py
+++ userprofiles/models.py
@@ -3,3 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    dni = models.IntegerField()
+    direccion = models.CharField(max_length=50)
+    telefono = models.IntegerField()
+    ciudad = models.CharField(max_length=50)
+    provincia = models.CharField(max_length=35)
+    pais = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Alumno(Persona):
+    carrera = models.CharField(max_length=30)
+    año = models.CharField(max_length=15)
+
