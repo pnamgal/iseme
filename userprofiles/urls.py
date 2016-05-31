65100ec1b40b6f77ba8ca70c27d69bfae3bd4121
pabli7o <pablito_dpunta@hotmail.com>
2016-05-31 16:55:16 -0300

    Mejora
    El registrar ya funciona

--- userprofiles/urls.py
+++ userprofiles/urls.py
@@ -4,9 +4,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^login/$', views.authentication, name= 'authentication'),
-    #url(r'^registrar/$', views.verificar, name= 'verificar'),
+    url(r'^registrar/$', views.verificar, name= 'verificar'),#en la url registrar..mira la funcion verificar
     url(r'^logout$', auth_views.logout, {'next_page': '/'}, name="logout"),
-    url(r'^registrar', views.registrar, name= "registrar"),
     url(r'^', views.hello, name= "hello"),
     
     ]  
--- userprofiles/views.py
+++ userprofiles/views.py
@@ -10,10 +10,7 @@ def authentication(request):
         action = request.POST.get('action', None)
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
-        
-        if action == 'signup':
-           user = User.objects.create_user(username=username, password=password)
-           user.save()   
+    
         if action == 'login':
             user = authenticate(username=username, password=password)
             login(request, user)
@@ -29,14 +26,15 @@ def verificar(request):
         action = request.POST.get('action', None)
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)  
-
+        
+        
         if action == 'signup':
            user = User.objects.create_user(username=username, password=password)
            user.save()   
         return redirect('/')
 
     context = {}
-    return render(request, 'login.html', context)
+    return render(request, 'registrar.html', context)
 #------------------------------------------------------
 #Muestra el template hello.html
 def hello(request):
